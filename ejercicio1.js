c1ad835303d710fdf0430c366e20f88a6fc93ea1
CandelaLucena <candelalucenareyes1212@gmail.com>
2022-12-15 11:38:04 +0100

    1 terminado

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,15 +1,19 @@
 window.onload = function(){
-    var formulario = document.getElementById("formulario");
-    formulario.addEventListener("submit", iniciarPrograma);
+    var formulario = document.getElementById("botonEnviar");
+    formulario.addEventListener("click", iniciarPrograma);
 }
 function iniciarPrograma(){
     var XMLHttpRequestObject = false;  
+    var nombre = document.getElementById("stringNombre").value;
+    var correo = document.getElementById("stringCorreo").value;
+    var datos = "nombre="+nombre+"&"+"correo="+correo;
 
     if (window.XMLHttpRequest) {
         XMLHttpRequestObject = new XMLHttpRequest();
     } else if (window.ActiveXObject) {
         XMLHttpRequestObject = new ActiveXObject("Microsoft.XMLHTTP");
     }
+
     if(XMLHttpRequestObject) {
         var objeto = document.getElementById("contenedor");
         XMLHttpRequestObject.open("POST", "fichero1.php");
@@ -18,6 +22,9 @@ function iniciarPrograma(){
                 objeto.innerHTML = XMLHttpRequestObject.responseText;
             }   
         }
-        XMLHttpRequestObject.send(null);
+        XMLHttpRequestObject.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        XMLHttpRequestObject.send(datos);
     }
+    document.getElementById("stringNombre").value="";
+    document.getElementById("stringCorreo").value="";
 }
--- fichero1.php
+++ fichero1.php
@@ -1,3 +1,27 @@
 <?php
+$opciones = array(
 
+    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+
+    PDO::ATTR_PERSISTENT => true
+
+);
+
+$pdo = new PDO(
+
+    'mysql:host=localhost;dbname=ajaxjavascript;charset=utf8',
+
+    'root',
+
+    'sa',
+
+$opciones);
+
+$nombre = $_POST["nombre"];
+$correo = $_POST["correo"];
+
+$sql = "INSERT INTO `contacto`(`nombre`, `correo`) VALUES ('[$nombre]','[$correo]')";
+$pdo->exec($sql);
 ?>
