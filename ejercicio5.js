89ac58890e7d84b8e42e77b2959fff8d6dbcd932
CandelaLucena <candelalucenareyes1212@gmail.com>
2023-01-23 12:33:14 +0100

    ejercicio 5 terminado

--- ejercicio5.js
+++ ejercicio5.js
@@ -1,7 +1,16 @@
 $(document).ready(function(){
     $("#boton").click(function(){
-        $.getJSON("https://restcountries.com/", function(data){
-
+        $.getJSON("https://reqres.in/api/users?page=1", function(data){
+            $.each( data, function(index, element) {
+                if(index=="data"){
+                    for (let i = 0; i < element.length; i++) {
+                        $("#primerDiv").append(element[i].id+" "+element[i].email+" "+element[i].first_name+" "+element[i].last_name+" "+element[i].avatar+"<br>");
+                    }
+                }else{
+                    $("#primerDiv").append(index);
+                    $("#primerDiv").append(element+"<br>");
+                }
+            });
         });
     });
 })
