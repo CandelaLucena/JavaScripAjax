cc12954e05b896c545182b8d4ee161cfd2b1a876
CandelaLucena <candelalucenareyes1212@gmail.com>
2023-01-26 13:50:24 +0100

    ejercicio3 terminado

--- ejercicio3.js
+++ ejercicio3.js
@@ -1,11 +1,13 @@
-$("#comenzar").click(function() {
+$("#comenzar").click(function moverse() {
     $("div")
-        .animate({ left: "+=200" }, 1000 )
-        .animate({ top: "+=200" }, 1000 )
-        .animate({ left: "-=200" }, 1000 )
-        .animate({ top: "-=200" }, 1000 )
+        .animate({ left: "200" }, 1000 )
+        .animate({ top: "200" }, 1000 )
+        .animate({ left: "0" }, 1000 )
+        .animate({ top: "0" }, 1000);
+        moverse();
+
 });
 
 $("#parar").click(function() {
-    $("div").stop();
+    $("div").stop(true);
 });
